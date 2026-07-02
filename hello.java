fcf77cd2142dcb2335c121c5b35a2725091dba7f
Karthik K <karthikkarthik13179@gmail.com>
2026-07-02 15:21:45 +0530

    karthik

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
-public class hello{
-  public static void main(String[] args){
-    System.out.println("hello from jenkins")
-      }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins");
+    }
 }
-  
